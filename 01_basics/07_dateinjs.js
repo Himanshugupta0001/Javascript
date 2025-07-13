2da9634d271d0ee24d5da6c7d32df7b984e60624
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-07-13 21:40:36 +0000

    date

---
+++ 01_basics/07_dateinjs.js
@@ -0,0 +1,49 @@
+// Date
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toISOString());
+
+// console.log(typeof myDate); // object
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString);
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+// console.log(myCreatedDate.toLocaleString);
+
+// let myCreatedDate = new Date("2023-01-14");
+// console.log(myCreatedDate.toLocaleString);
+
+let myCreatedDate = new Date("01-14-2023");
+// console.log(myCreatedDate.toLocaleString);
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000)); // to convert into seconds
+
+let newDate = new Date();
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
+
+
+
+
+
+
+
+
+
+
+
+
