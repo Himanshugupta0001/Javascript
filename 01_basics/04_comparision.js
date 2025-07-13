cd48cf4081d94da7df020d7679a14800e2a5d47b
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-07-13 12:15:04 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); //true
+// console.log("02" > 1); // true
+
+console.log(null > 0);  //false
+console.log(null == 0); //false
+console.log(null >= 0);  //true
+
+console.log(undefined == 0); // false
+console.log(undefined < 0); // false
+console.log(undefined > 0); // false
+  
+
+// === strict check this also check datatype
+
+console.log("2" === 2); //in this it will tell that the datatype is not same
