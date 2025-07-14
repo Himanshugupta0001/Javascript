009be6838fb82914849c3bcc6876062a7c2f4ee5
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-07-14 20:32:34 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,27 +7,27 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('himanshuuu-nn-aa')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+console.log(gameName[0]);  //h
+console.log(gameName.__proto__); // {}
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(3));
 console.log(gameName.indexOf('s'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+const newString = gameName.substring(0, 4) //basically substring de degha starting kaha se aur ending kaha tak ka
+console.log(newString);  //hima
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-8, 4)  //negative dene par reverse se start hogha ye bhi substring hi degha
 console.log(anotherString);
 
 const newStringOne = "     hiamshu     "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne);   //isme space bhi degha 
+console.log(newStringOne.trim()); //isme space nhi degha
 
 
 const url = "https://himanshu.com/himanshu%20gupta"
-console.log(url.replace('%20', '-'));
-console.log(url.includes('himanshu'));
+console.log(url.replace('%20', '-')); //ye replace kar degha uski jagah
+console.log(url.includes('himanshu')); //ye check karta hai ki string include kar raha ya nhi
 
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-')); //ye spilt kar degha
