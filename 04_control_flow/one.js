83696648219e72686175e0e77ea28ba2736696a1
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-07-14 19:32:50 +0000

    if and conditon

---
+++ 04_control_flow/one.js
@@ -0,0 +1,49 @@
+//if
+
+// const isUserLoggedIn = true
+// const temperature = 41
+// if(temperature < 50){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("temperature is greater than 50");
+// }
+
+//<, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`Use Power: ${power}`);
+// }
+
+// console.log(`Use Power: ${power}`);
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2"); //this is not good way to write code
+
+
+// if(balance < 500){
+//     console.log("less than 500");
+// }
+// else if(balance < 750){
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("greater than 750");
+// }
+
+const UserLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(UserLoggedIn && debitcard && 2==2){
+    console.log("Allow to buy the course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
