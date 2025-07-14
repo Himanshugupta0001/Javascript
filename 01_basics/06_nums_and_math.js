3686bbb21bac4f2af4db6169a45212cfc11e9715
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-07-14 20:42:00 +0000

    Math and num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,17 +1,19 @@
 const score = 400
+//console.log(score); //400 degha
+
 
 const balance = new Number(100)
-// console.log(balance);
+// console.log(balance);  //[Number: 100] degha
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2)); //precise ke liye hai andar number jitna denghe utne tak wo . ke baad tak ka ans degha
 
 const otherNumber = 23.6596
 
-// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3)); // ye round off kar degha jitna denghe
 
 const hundreds = 1000000
-//console.log(hundreds.toLocaleString('en-IN'));
+//console.log(hundreds.toLocaleString('en-IN')); //ye commas laga dete hai
  
 
 // ++++++++++++++++++++Maths+++++++++++++++++++//
@@ -24,7 +26,7 @@ const hundreds = 1000000
 // console.log(Math.max(4, 3, 6, 8)); //8
 
 
-console.log(Math.random());
+console.log(Math.random());   //iski hamesa value 0-1 me hi aayeghi
 console.log((Math.random()*10) + 1); 
 console.log((Math.random()*10) + 1); 
 
