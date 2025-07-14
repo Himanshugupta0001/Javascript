28de05c8b6dc99c44a99ee35c7ee8cde3485f635
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-07-14 09:11:45 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,6 +25,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*****************Operations****************/
+let value = 3
+let negValue = -value
+console.log(negValue);  //-3
+
+// console.log(2+2); //4
+// console.log(2-2); //0 
+// console.log(2*2);  //4
+// console.log(2**2); //2 power 2 
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "himashu"
+let str2 = "gupta"
+
+let str3 = str1 + str2
+console.log(str3); // hiamshu gupta
+
+console.log("1" + 2);   // 12
+console.log(1 + "2");   // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); //101
+
 
