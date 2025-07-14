cbbe787ceafb88d26591b6336a60abfdbbd7fdce
Himanshugupta0001 <gupta.himanshu2324@gmail.com>
2025-07-14 11:22:32 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,58 @@
+const user = {
+    username : "himanshu",
+    price : 999,
+
+    welcomeMessage : function(){
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);   // ye {} print karegha
+
+// function chai(){
+//     let username = "himanshu"
+//     console.log(this.username); //ye undefined degha function ke andar this use kar rahe hai isilie
+// }
+
+// chai()
+
+// const chai = function(){
+//     let username = "himanshu"
+//     console.log(this.username) // ye bhi undefined hi degha
+// }
+// chai()
+
+
+// const chai = () => {
+//     let username = "himanshu"
+//     console.log(this.username) // ye bhi undefined hi degha
+// }
+// chai()
+
+const chai = () => {
+    let username = "himanshu"
+    console.log(this) // ye {} degha
+}
+//chai()
+
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// console.log(addTwo(3, 4));  // ye 7 degha
+
+
+// const addTwo = (num1, num2) =>  num1 + num2
+// console.log(addTwo(3, 4)); //ye nhi 7 de degha
+
+// const addTwo = (num1, num2) =>  (num1 + num2)
+// console.log(addTwo(3, 4)); //ye nhi 7 de degha
+
+const addTwo = (num1, num2) =>  ({username: "himanshu"})
+console.log(addTwo(3, 4));  //ye {username: "himanshu"} degha
